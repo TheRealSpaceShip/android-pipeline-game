6e5f57bbed3577eaec079f17a8f2caadb414e2f0
sharlaimov@gmail.com <sharlaimov@gmail.com@a7bf3939-2b71-f395-ac95-96edcf994e9b>
2012-01-24 16:30:04 +0000

    added simple  pipes .png textures empty SurfaceView

---
+++ trunk/src/sod/games/pipeline/SewerageView.java
@@ -0,0 +1,19 @@
+package sod.games.pipeline;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class SewerageView extends SurfaceView {
+	public SewerageView(Context context) {
+		super(context);
+	}
+
+	
+	@Override
+	public void draw(Canvas canvas) {
+		super.draw(canvas);
+	}
+	
+	
+}
